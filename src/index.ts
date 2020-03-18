ba31ef792bf427a8cc0c4ae99cbdd347542ebfc0
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-03-18 20:18:46 +0000

    Observer y subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
+const observer: Observer<string> = {
+    next(value) {
+        const p = document.createElement('p');
+        p.innerText = value;
+        document.body.appendChild(p);
+    },
+    error(error) {
+        alert(error)
+    },
+    complete() {
+        document.body.style.backgroundColor = 'black';
+    }
+};
 
 const obs$ = new Observable<string>(subscriber => {
     subscriber.next('Hola');
     subscriber.next('Mundo');
 
+    // const a = undefined;
+    // a.nombre = 'Fernando';
+
     subscriber.complete();
 });
 
-obs$.subscribe(resp => {
-    const p = document.createElement('p');
-    p.innerText = resp;
-    document.body.appendChild(p);
-});
+obs$.subscribe(observer);
