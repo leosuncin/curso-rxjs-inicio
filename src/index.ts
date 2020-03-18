09b81f6b0772ede83d8a1fde52643c9addb11327
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-03-18 23:13:01 +0000

    range

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import { fromEvent } from 'rxjs';
+import { range, asyncScheduler } from 'rxjs';
 
-const src1$ = fromEvent<MouseEvent>(document, 'click');
-const src2$ = fromEvent<KeyboardEvent>(document, 'keyup')
+const src$ = range(1, 100, asyncScheduler);
 
-src1$.subscribe(event => {
-    const app = document.getElementById('app');
-    app.innerText = `position: (${event.x}, ${event.y})`;
-});
-src2$.subscribe(event => {
-    const app = document.getElementById('app');
-    app.innerText = `key: ${event.key}`;
+src$.subscribe(value => {
+    document.getElementById('app').innerText += ', ' + value;
 });
